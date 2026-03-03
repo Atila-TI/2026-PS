62f74249109b244a0fcd8f723fee0f9848b0b56a
Atila-TI <20251ctb0100045@estudantes.ifpr.edu.br>
2026-03-03 12:13:27 +0000

    teste

---
+++ 03_estruturas/02-desafio.py
@@ -0,0 +1,10 @@
+# =========================================================
+# SISTEMA DE BIBLIOTECA
+# =========================================================
+# Disciplina : Programação de Sistemas (PS)
+# Aula       : 05 - Revisão: Estruturas de Dados
+# Autor      : [Atila do Nascimento Pereira]
+# Data       : [26/02/2026]
+# Repositório: https://github.com/[Atila-TI]/2026-PS
+# =========================================================
+# =========================================================
